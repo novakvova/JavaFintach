70405a75c0cc6dce76df7887ceb87a2edc0782cd
novakvova <novakvova@gmail>
2024-12-15 12:00:20 +0200

    Begin connection database

--- 2.OOP/src/main/java/org/example/Main.java
+++ 2.OOP/src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello App :)");
+        String host = "ep-still-morning-a2sdufws.eu-central-1.aws.neon.tech";
+        String url = "jdbc:postgresql://"+host+":5432/neondb";
+        String user = "neondb_owner";
+        String password = "oh5pxmtGZN0J";
+
+        // Initialize the connection
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            //e.printStackTrace();
+        }
     }
 }
