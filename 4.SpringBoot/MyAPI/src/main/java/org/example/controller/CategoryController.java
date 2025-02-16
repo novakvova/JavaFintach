c9452cea9bc8cc25ffe225afec2168114dc93537
novakvova <novakvova@gmail>
2025-02-16 09:47:36 +0200

    CRUD Category Complete

--- 4.SpringBoot/MyAPI/src/main/java/org/example/controller/CategoryController.java
+++ 4.SpringBoot/MyAPI/src/main/java/org/example/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package org.example.controller;
 
+import org.example.dto.category.CategoryEditDTO;
 import org.example.dto.category.CategoryPostDTO;
 import org.example.entities.CategoryEntity;
 import org.example.service.CategoryService;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
@@ -20,13 +22,30 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public List<CategoryEntity> getAllCategories() {
-        return categoryService.getAllCategories();
+    public List<CategoryEntity> getAll() {
+        return categoryService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryEntity> getCategoryById(@PathVariable Integer id) {
+        Optional<CategoryEntity> category = categoryService.getById(id);
+        return category.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping(consumes = MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<CategoryEntity> createCategory(@ModelAttribute CategoryPostDTO categoryPostDTO) {
-        CategoryEntity category = categoryService.createCategory(categoryPostDTO);
+    public ResponseEntity<CategoryEntity> create(@ModelAttribute CategoryPostDTO categoryPostDTO) {
+        CategoryEntity category = categoryService.create(categoryPostDTO);
         return new ResponseEntity<>(category, HttpStatus.CREATED);
     }
+
+    @PutMapping(consumes = MULTIPART_FORM_DATA_VALUE)
+    public CategoryEntity update(@ModelAttribute CategoryEditDTO category) {
+        return categoryService.edit(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        categoryService.delete(id);
+    }
+
 }
--- 4.SpringBoot/MyAPI/src/main/java/org/example/service/CategoryService.java
+++ 4.SpringBoot/MyAPI/src/main/java/org/example/service/CategoryService.java
@@ -1,5 +1,6 @@
 package org.example.service;
 
+import org.example.dto.category.CategoryEditDTO;
 import org.example.dto.category.CategoryPostDTO;
 import org.example.entities.CategoryEntity;
 import org.example.repository.CategoryRepository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -17,11 +19,15 @@ public class CategoryService {
     @Autowired
     private FileService fileService;
 
-    public List<CategoryEntity> getAllCategories() {
+    public List<CategoryEntity> getAll() {
         return categoryRepository.findAll();
     }
 
-    public CategoryEntity createCategory(CategoryPostDTO categoryPostDTO) {
+    public Optional<CategoryEntity> getById(Integer id) {
+        return categoryRepository.findById(id);
+    }
+
+    public CategoryEntity create(CategoryPostDTO categoryPostDTO) {
         CategoryEntity categoryEntity = new CategoryEntity();
         categoryEntity.setName(categoryPostDTO.getName());
         var imgName = fileService.load(categoryPostDTO.getImageFile());
@@ -29,6 +35,37 @@ public class CategoryService {
         categoryEntity.setDescription(categoryPostDTO.getDescription());
         categoryEntity.setCreationTime(LocalDateTime.now());
         return categoryRepository.save(categoryEntity);
+    }
+
+    public CategoryEntity edit(CategoryEditDTO dto) {
+        CategoryEntity entity = categoryRepository.findById(dto.getId())
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        if (dto.getName() != null && !dto.getName().isBlank()) {
+            entity.setName(dto.getName());
+        }
+
+        if (dto.getDescription() != null && !dto.getDescription().isBlank()) {
+            entity.setDescription(dto.getDescription());
+        }
+
+        var newImageFile = dto.getImageFile();
+        if (newImageFile!=null && !newImageFile.isEmpty()){
+            var newImagePath = fileService.replace(entity.getImage(), dto.getImageFile());
+            entity.setImage(newImagePath);
+        }
+
+        return categoryRepository.save(entity);
+    }
+
+    public boolean delete(Integer id) {
+        var res = getById(id);
+        if (res.isEmpty()){
+            return false;
+        }
+        fileService.remove(res.get().getImage());
+        categoryRepository.deleteById(id);
+        return true;
     }
 }
 
